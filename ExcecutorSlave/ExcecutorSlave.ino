56b4ea850c75ad5ae2a8a7218630dd98c6236a3f
Richard Hausman <hausman.richard@gmail.com>
2017-04-19 18:43:53 -0400

    Added alarm clock, Lock support
    piezo on pin 12, pushbutton (snooze button) on pin 13

--- ExcecutorSlave/ExcecutorSlave.ino
+++ ExcecutorSlave/ExcecutorSlave.ino
@@ -1,3 +1,5 @@
+#include <Time.h>
+
 // ExecutorSlave module code for ApertureAutomation
 //Copyright under Aperture Science on April 13, 2017
 #include <Servo.h>
@@ -28,25 +30,45 @@ KEY */
 int lights[3] = {2,5,6};//pins. Position in array is the light's number, starting with light 0
 //consider slider for lights so that they can be dimmed
 Servo lock , petFeeder;
-//Servo servos[2] = {lock,feeder}; if we have more servos, we'll use array structure
+Servo servos[2] = {lock,petFeeder};
 
 IRsend irSend; // irSend.sendSony(code,numberOfBits). The hex codes are in a txt file in this folder, and the #ofBits is the number of hex digits times 4
 //remember that sony makes you send the code thrice
 
-
+#define PIEZO 12
+#define SNOOZE 13
+int alarmTimes[3][2]=//first array is array of alarms, sub array is {hour,minute} in 24 hour format
+{//3 alarms: 7:00 am, 7:30 am, 13:01 aka 1:01 pm
+  {7,00} , {7,30} , {13,01}
+  };
+//time_t last;
+/*******************************************************************************/
 void setup()
 {
   Wire.begin(8);                // join i2c bus with address #8
   Wire.onReceive(receiveEvent); // register event
   Serial.begin(9600);
+  //pinModes
+  pinMode(PIEZO, OUTPUT);
+  pinMode(SNOOZE, INPUT); //don't forget a 10k resistor
   for(int i=0; i < sizeof(lights)/sizeof(int);i++) {pinMode(lights[i], OUTPUT); Serial.println("LED INITIALIZED");}
   for(int i=0; i < sizeof(servos)/sizeof(Servo);i++) {servos[i].attach(i+7); Serial.println("SERVO INITIALIZED");} //SERVOS WILL BE ON PINS 7,8,9, etc.
-}
+}                                                                                                        //note to self: check that servos are on pwm pins
 
 void loop()
 {
+  if(int s= second() < 3)//I'm too lazy to do a check for how recently the alarm went off
+    {
+    //for a< number of alarms
+    for(int a=0;a<3;a++)
+    {
+      int h = hour();
+      int m = minute();
+      if (alarmTimes[a][0] == h && alarmTimes[a][1] == m) alarm();//if the hour and minute match, alarm
+        }//for
+    }//if seconds < 3
   delay(100);
-}
+}//loop
 
 // function that executes whenever data is received from master
 // this function is registered as an event, see setup()
@@ -90,13 +112,14 @@ void execute(struct parcel p)
                         default: Serial.println("unidentifyable tv command");
                        }//TV subswitch ending
      case 's' ://servo
-              Serial.println(F("Setting servo state") + p.command[2] == 'f'?"forward":"neutral");
+              Serial.print(F("Setting servo state: ")); Serial.println(p.command[2] == 'f'?"forward":"neutral");
               //servos[p.command[1]].write(p.command[2]);
               if(p.command[1]=='p') adjustServo(petFeeder,p.command[2]);
+              if(p.command[1] == 'l') adjustServo(lock,p.command[2]);
               
               break;
 
-     default: Serial.print("UNACCEPTABLE PARCEL: "); for(int(i=0;i<3;i++) {Serial.println(p.command[i]);} Serial.println();
+     default: Serial.print("UNACCEPTABLE PARCEL: "); for(int i=0;i<3;i++) {Serial.println(p.command[i]);} Serial.println();
                  
      }//switch
                     
@@ -106,4 +129,11 @@ void adjustServo(Servo s, char c)
 {
   if(c=='f'){s.write(90);} else {s.write(90);}
   delay(15);
+  }
+
+void alarm() //set off piezo, stop when button pressed
+{
+  digitalWrite(PIEZO,HIGH);
+  while(digitalRead(SNOOZE) == 0) {;}//will keep checking until buttonis pressed
+  //last = now();//mark when the last alarm went off
   }
